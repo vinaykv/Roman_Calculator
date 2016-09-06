f44bbf7c6737bb52a6f2f192f3af91e4e88780ea
vinaykv <vinaykva@gmail.com>
2016-09-06 09:56:43 -0700

    added README file contents

--- calculator.c
+++ calculator.c
@@ -1,3 +1,10 @@
+/* 
+Roman calculator: performs addition and subtraction of roman values
+Author: Vinay vittal Karagod
+File dependents: main.c, calculator.h, calculator.c,calculator-test.check
+Date: 09/06/2016
+*/
+
 # include "calculator.h"
 # include <check.h>
 # include <stdio.h>
